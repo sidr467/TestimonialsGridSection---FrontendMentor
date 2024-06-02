c1743274753548d58cc3d8e5885e0c997770829b
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-06-02 17:09:14 +0530

    Added custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      Moderateviolet: "hsl(263, 55%, 52%)",
+      Verydarkgrayishblue: "hsl(217, 19%, 35%)",
+      Verydarkblackishblue: "hsl(219, 29%, 14%)",
+      White: "hsl(0, 0%, 100%)",
+      Lightgray: "hsl(0, 0%, 81%)",
+      Lightgrayishblue: "hsl(210, 46%, 95%)",
+    },
   },
   plugins: [],
 }
